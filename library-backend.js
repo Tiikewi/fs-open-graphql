f0b77f5e41dec08bb41aa12946b300226fd0c7b2
Kimi <porthan.kimi@gmail.com>
2023-01-18 23:12:30 +0200

    8.4 & 8.5 done

--- library-backend.js
+++ library-backend.js
@@ -110,7 +110,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -120,7 +120,18 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount : () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author && !args.genre) return books
+      if (args.author !== undefined && args.genre !== undefined) {
+        return (
+        books.filter(book => book.author === args.author).
+        filter(book => book.genres.includes(args.genre))
+        )
+      }
+      if(args.genre) return books.filter(book => book.genres.includes(args.genre))
+      
+      return books.filter(book => book.author === args.author)
+    },
     allAuthors: (root) => authors
   },
   Author: {
